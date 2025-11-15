14bbc3b966ada8d39d6782ffe80fe601905ba5e9
Alberto Minetti <alberto.minetti@gmail.com>
2025-11-15 12:08:35 +0100

    starting with day 3

---
+++ 2025/day-03/day-03.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import re
+import math
+import itertools
+import tqdm
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+file = open("test-input-03-01.txt").read()
+print(file)
+
+
