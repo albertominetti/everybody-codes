175dc572fcd9c7a1686d8190af5d93b8787a6ee1
Alberto Minetti <alberto.minetti@gmail.com>
2025-11-15 12:13:31 +0100

    day 3 part 1 completed

--- 2025/day-03/day-03.py
+++ 2025/day-03/day-03.py
@@ -8,7 +8,10 @@ import tqdm
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-file = open("test-input-03-01.txt").read()
+file = list(map(int, open("test-input-03-01.txt").read().split(",")))
 print(file)
 
+crates = sorted(list(set(file)), reverse=True)
+print(crates)
 
+print(sum(crates))
